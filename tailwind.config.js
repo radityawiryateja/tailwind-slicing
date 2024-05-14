3506d4e1a75ae60de6f7ea8fa61c0ba0c3737011
radityawiryateja <radityawiryateja62@gmail.com>
2024-05-14 23:38:05 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      main: {
+        0: "white",
+        1: "#E9E3FF",
+        2: "#4318FF",
+        3: "#68769F",
+        4: "#1B2559",
+        5: "#F6F8FD",
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+};
